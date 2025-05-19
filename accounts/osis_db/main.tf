a0c3175c8ffd5f7734747152de84f8798e4f3ffe
Riyaz <mfriyaz@gmail.com>
2025-05-19 13:18:19 +0800

    IP access for Amin use

--- accounts/osis_db/main.tf
+++ accounts/osis_db/main.tf
@@ -4,7 +4,7 @@ module "osis_mssql_sg" {
   name        = "ec2-mssql-access"
   description = "Allow MSSQL access"
   vpc_id      = "vpc-01541d8a67c16ca49"
-  allowed_cidrs = ["106.219.182.3/24"]
+  allowed_cidrs = ["106.219.182.3/32"]
   tags = {
     Environment = "OSIS_DB"
     ManagedBy   = "Terraform"
