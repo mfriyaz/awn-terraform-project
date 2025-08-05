36f5820aa92a8f0bc18b44bf04195f0cca3ced61
Riyaz <mfriyaz@gmail.com>
2025-08-05 19:19:47 +0800

    Initial Update

--- accounts/vmsnow_app/main.tf
+++ accounts/vmsnow_app/main.tf
@@ -4,7 +4,7 @@ module "ftp_sg" {
   name        = "ec2-ftp-access"
   description = "Allow FTP access"
   vpc_id      = "vpc-07cc34a429fb05169"
-  allowed_cidrs = ["106.219.183.156/32"]
+  allowed_cidrs = ["106.219.183.157/32"]
   tags = {
     Environment = "VMSNOW_APP"
     ManagedBy   = "Terraform"
