f202a02a2153224c1ea1215efd50b3582b3112bb
mfriyaz <84617147+mfriyaz@users.noreply.github.com>
2025-09-22 21:35:02 +0800

    Developer access

--- accounts/svs_app/main.tf
+++ accounts/svs_app/main.tf
@@ -4,7 +4,7 @@ module "ftp_mssql_sg" {
   name        = "ec2-ftp-mssql-access"
   description = "Allow FTP and MSSQL access"
   vpc_id      = "vpc-0770331a6f17a5f46"
-  allowed_cidrs = ["192.60.10.0/24"]
+  allowed_cidrs = ["106.219.182.38/32"]
   tags = {
     Environment = "SVS_APP"
     ManagedBy   = "Terraform"
