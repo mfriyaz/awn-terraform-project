1884ae4f21b331de26d23991a67a049a6ac035b5
Riyaz <mfriyaz@gmail.com>
2025-12-24 15:29:46 +0800

    Developer Access

--- accounts/osis_db/main.tf
+++ accounts/osis_db/main.tf
@@ -4,7 +4,7 @@ module "osis_mssql_sg" {
   name        = "ec2-mssql-access"
   description = "Allow MSSQL access"
   vpc_id      = "vpc-01541d8a67c16ca49"
-  allowed_cidrs = ["106.219.182.239/32"]
+  allowed_cidrs = ["122.167.100.75/32"]
   tags = {
     Environment = "OSIS_DB"
     ManagedBy   = "Terraform"
