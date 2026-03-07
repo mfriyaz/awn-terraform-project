172c88e6790379a4793774f646db3a83ffb1b9c5
mfriyaz <84617147+mfriyaz@users.noreply.github.com>
2026-03-07 14:34:43 +0800

    Update main.tf

--- accounts/uat_db/main.tf
+++ accounts/uat_db/main.tf
@@ -4,7 +4,7 @@ module "ftp_mssql_sg" {
   name        = "ec2-ftp-mssql-access"
   description = "Allow FTP and MSSQL access"
   vpc_id      = "vpc-09df8e4d092501997"
-  allowed_cidrs = ["49.37.210.6/32"]
+  allowed_cidrs = ["49.37.213.20/32"]
   tags = {
     Environment = "UAT_DB"
     ManagedBy   = "Terraform"
