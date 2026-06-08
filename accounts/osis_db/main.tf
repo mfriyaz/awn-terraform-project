6391390c7c74c1652fe9479e3789a319326aaaf4
mfriyaz <84617147+mfriyaz@users.noreply.github.com>
2026-06-08 17:06:14 +0800

    Update main.tf

--- accounts/osis_db/main.tf
+++ accounts/osis_db/main.tf
@@ -4,7 +4,7 @@ module "osis_mssql_sg" {
   name        = "ec2-mssql-access"
   description = "Allow MSSQL access"
   vpc_id      = "vpc-01541d8a67c16ca49"
-  allowed_cidrs = ["49.37.215.106/32"]
+  allowed_cidrs = ["49.37.211.142/32"]
   tags = {
     Environment = "OSIS_DB"
     ManagedBy   = "Terraform"
